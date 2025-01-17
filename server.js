bcf96ec8d1c5064fed2ace05768f72a292d1df12
Julio <beito_xp@me.com>
2025-01-17 10:47:18 -0600

    Express Integrated

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const distFolder = path.join(__dirname, 'dist/competencias-frontend');
 app.use(express.static(distFolder));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(distFolder, 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist/competencias-frontend/index.html'));
 });
 
 const PORT = process.env.PORT || 8080;
